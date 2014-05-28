4722f39c08315ff20fdee634e62a386f56f686ed
shittc <shittc@Parkui-MacBook-Air.local>
2014-05-29 08:24:57 +0900

    Initial Commit

---
+++ iContact/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iContact
+//
+//  Created by Park Jun Sung on 2014. 5. 29..
+//  Copyright (c) 2014년 Junsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iContact/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iContact
+//
+//  Created by Park Jun Sung on 2014. 5. 29..
+//  Copyright (c) 2014년 Junsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
